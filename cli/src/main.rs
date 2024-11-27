587dfef6561b9d69f1f8384238492e2c1101eb7f
marekzan <marekzan25@gmail.com>
2024-11-27 18:28:11 +0100

    implement notify to cli app

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,7 @@
-use neveno_core::config::AppConfig;
+use neveno_core::sink::SinkNotifier;
+use std::collections::HashMap;
+
+use neveno_core::{checker, config::AppConfig};
 
 use anyhow::Result;
 use clap::Parser;
@@ -14,7 +17,25 @@ struct Cli {
 async fn main() -> Result<()> {
     let cli = Cli::parse();
     let config = AppConfig::load(&cli.config)?;
-    // checker::check(&config).await?;
-    println!("{:?}", config);
+
+    let notifiers: HashMap<String, Box<dyn SinkNotifier>> = config
+        .notifiers
+        .into_iter()
+        .map(|notifier| (notifier.name, notifier.sink.to_notifier()))
+        .collect();
+
+    for app in &config.applications {
+        if let Some(latest_version) = checker::check(app).await? {
+            app.notifier
+                .iter()
+                .try_for_each(|notifier_name| -> Result<()> {
+                    if let Some(notifier) = notifiers.get(notifier_name) {
+                        println!("Sending notification to {}", notifier_name);
+                        notifier.send(&latest_version)?;
+                    }
+                    Ok(())
+                })?;
+        }
+    }
     Ok(())
 }
