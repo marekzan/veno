e9ef221e860d331ae841dd2e76a617f62fb81130
marekzan <marekzan25@gmail.com>
2024-11-28 14:40:34 +0100

    refactor sink

--- core/src/config.rs
+++ core/src/config.rs
@@ -2,9 +2,7 @@ use anyhow::{Context, Result};
 use config::{Config, File, FileFormat};
 use serde::Deserialize;
 
-use crate::sink::{
-    email::EmailNotifier, google_chat::GoogleChatNotifier, slack::SlackNotifier, SinkNotifier,
-};
+use crate::sink::Sink;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct AppConfig {
@@ -23,46 +21,6 @@ pub struct Application {
 pub struct Notifier {
     pub name: String,
     pub sink: Sink,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-#[serde(tag = "type")] // Use tag-based enum for sink type
-pub enum Sink {
-    #[serde(rename = "slack")]
-    Slack { webhook: String },
-    #[serde(rename = "email")]
-    Email {
-        host: String,
-        port: u16,
-        username: String,
-        password: String,
-    },
-    #[serde(rename = "google_chat")]
-    GoogleChat { webhook: String },
-}
-
-impl Sink {
-    pub fn to_notifier(&self) -> Box<dyn SinkNotifier> {
-        match self {
-            Sink::Slack { webhook } => Box::new(SlackNotifier {
-                webhook: webhook.clone(),
-            }),
-            Sink::Email {
-                host,
-                port,
-                username,
-                password,
-            } => Box::new(EmailNotifier {
-                host: host.clone(),
-                port: *port,
-                username: username.clone(),
-                password: password.clone(),
-            }),
-            Sink::GoogleChat { webhook } => Box::new(GoogleChatNotifier {
-                webhook: webhook.clone(),
-            }),
-        }
-    }
 }
 
 impl AppConfig {
--- core/src/sink/mod.rs
+++ core/src/sink/mod.rs
@@ -1,10 +1,58 @@
 use anyhow::Result;
+use email::EmailNotifier;
+use google_chat::GoogleChatNotifier;
+use serde::Deserialize;
+use slack::SlackNotifier;
+use std::{future::Future, pin::Pin};
 
 pub mod email;
 pub mod google_chat;
 pub mod slack;
 
+#[derive(Deserialize, Debug, Clone)]
+#[serde(tag = "type")] // Use tag-based enum for sink type
+pub enum Sink {
+    #[serde(rename = "slack")]
+    Slack { webhook: String },
+    #[serde(rename = "email")]
+    Email {
+        host: String,
+        port: u16,
+        username: String,
+        password: String,
+    },
+    #[serde(rename = "google_chat")]
+    GoogleChat { webhook: String },
+}
+
+impl Sink {
+    pub fn to_notifier(&self) -> Box<dyn SinkNotifier<Output = String>> {
+        match self {
+            Sink::Slack { webhook } => Box::new(SlackNotifier {
+                webhook: webhook.clone(),
+            }),
+            Sink::Email {
+                host,
+                port,
+                username,
+                password,
+            } => Box::new(EmailNotifier {
+                host: host.clone(),
+                port: *port,
+                username: username.clone(),
+                password: password.clone(),
+            }),
+            Sink::GoogleChat { webhook } => Box::new(GoogleChatNotifier {
+                webhook: webhook.clone(),
+            }),
+        }
+    }
+}
+
 pub trait SinkNotifier: Send + Sync {
-    fn send(&self, message: &str) -> Result<()>;
-    fn type_name(&self) -> &str;
+    type Output;
+    fn send<'a>(
+        &'a self,
+        message: &'a str,
+    ) -> Pin<Box<dyn Future<Output = Result<Self::Output>> + Send + 'a>>;
 }
