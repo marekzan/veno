ad5ab8e9c90519e123f3aebb3335af94102c0793
marekzan <marekzan25@gmail.com>
2024-12-30 17:40:50 +0100

    added first draft of email functionality

--- core/src/notifier/email.rs
+++ core/src/notifier/email.rs
@@ -1,3 +1,8 @@
+use anyhow::Result;
+use lettre::{
+    message::header::ContentType, transport::smtp::authentication::Credentials, Message,
+    SmtpTransport, Transport,
+};
 use serde::Deserialize;
 
 use super::SinkSender;
@@ -5,13 +10,61 @@ use super::SinkSender;
 #[derive(Deserialize, Clone, Debug)]
 pub struct EmailSink {
     pub host: String,
-    pub port: u16,
+    pub port: Option<u16>,
     pub username: String,
     pub password: String,
+    pub to: String,
 }
 
 impl SinkSender for EmailSink {
     async fn send(&self, message: &str) {
-        println!("Email sent: {}", message);
+        let email = match create_message(&self.username, &self.to, message) {
+            Ok(email) => email,
+            Err(e) => {
+                eprintln!("Failed to create email: {:?}", e);
+                return;
+            }
+        };
+
+        let mailer = match create_mailer(&self.host, self.port, &self.username, &self.password) {
+            Ok(mailer) => mailer,
+            Err(e) => {
+                eprintln!("Failed to create mailer: {:?}", e);
+                return;
+            }
+        };
+
+        // Send the email
+        match mailer.send(&email) {
+            Ok(_) => println!("Email sent successfully!"),
+            Err(e) => eprintln!("Failed to send email: {:?}", e),
+        }
     }
+}
+
+fn create_message(from: &str, to: &str, message: &str) -> Result<Message> {
+    let email = Message::builder()
+        .from(format!("VENO <{}>", from).parse()?)
+        .to(to.parse()?)
+        .subject("VENO: New version available!")
+        .header(ContentType::TEXT_PLAIN)
+        .body(message.to_string())
+        .unwrap();
+
+    Ok(email)
+}
+
+fn create_mailer(
+    host: &str,
+    port: Option<u16>,
+    username: &str,
+    password: &str,
+) -> Result<SmtpTransport> {
+    let creds = Credentials::new(username.to_string(), password.to_string());
+    let mailer = SmtpTransport::starttls_relay(host)?
+        .credentials(creds)
+        .port(port.unwrap_or(587))
+        .build();
+
+    Ok(mailer)
 }
