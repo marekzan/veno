2ff6624449719c0793f4f238eacba7b52375217f
marekzan <marekzan25@gmail.com>
2025-04-25 10:37:01 +0200

    add todo

--- veno-core/src/lib.rs
+++ veno-core/src/lib.rs
@@ -8,6 +8,8 @@ use std::{sync::LazyLock, time::Duration};
 use anyhow::Result;
 use reqwest::{Client, ClientBuilder, Response};
 
+// TODO this panics during runtime! it would be better to exit on startup. Maybe use OnceLock?
+// or initialize the client in main?
 static CLIENT: LazyLock<Client> = LazyLock::new(|| {
     ClientBuilder::new()
         .user_agent("veno")
